300987056a09b258f7fa68333fd5ff7ad908441d
progspace1204 <progspace1204@gmail.com>
2024-03-23 09:39:07 -0700

    fix: the layout in curseofthe

--- src/views/Games/Curseof/Survive.tsx
+++ src/views/Games/Curseof/Survive.tsx
@@ -5,7 +5,7 @@ const Survive: React.FC = () => {
   return (
     <section className="w-full h-full relative">
       <div className="w-full h-full overflow-hidden px-5">
-        <div className="max-w-[1400px] w-full mx-auto flex justify-between lg:flex-row flex-col sm:items-center items-start">
+        <div className="container w-full mx-auto flex justify-between lg:flex-row flex-col sm:items-center items-start">
           <motion.div
             initial={{ x: -150 }}
             whileInView={{ x: 0 }}
