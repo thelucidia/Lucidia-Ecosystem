56e0aa07c4f61ab98fdff01efcdc2558a3c6c417
progspace1204 <progspace1204@gmail.com>
2024-03-26 06:36:34 -0700

    fix: update home baner in app pages

--- src/views/Apps/Home/Banner.tsx
+++ src/views/Apps/Home/Banner.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsTwitterX } from 'react-icons/bs';
 import { LiaTelegramPlane } from 'react-icons/lia';
 import { RxDiscordLogo } from 'react-icons/rx';
 import { TfiWorld } from 'react-icons/tfi';
+import MainVideo from '../../../../videos/home_app.mp4';
 import Detail from './Detail';
 
 const Banner: React.FC = () => {
@@ -24,13 +25,40 @@ const Banner: React.FC = () => {
       icon: <TfiWorld />,
     },
   ];
+
+  const [trueLoading, setTrueLoading] = useState(true);
+
+  const handleTrue = () => {
+    setTimeout(() => {
+      setTrueLoading(false);
+    }, 1500);
+  };
+
   return (
     <section className="w-full min-h-screen relative text-white overflow-hidden bg-bg">
-      <img
+      <div
+        className={`w-full h-screen bg-black fixed z-30 top-0 left-0 flex flex-col items-center justify-center  transition-all ease-in-out duration-300 ${trueLoading ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'} `}
+      >
+        <div className={`animate-pulse flex justify-center items-center flex-col`}>
+          <img src="/loading.svg" alt="Logo" className="w-24" />
+          <h1 className="font-primary font-semibold text-2xl mt-10 tracking-widest">LOADING</h1>
+        </div>
+      </div>
+      <video
+        autoPlay
+        muted
+        loop
+        src={MainVideo}
+        preload={'auto'}
+        onLoadedData={handleTrue}
+        playsInline
+        className="w-full h-screen object-cover absolute top-0 left-0"
+      />
+      {/* <img
         src="/assets/app/games/curse.jpg"
         alt="game_background"
         className="w-full h-screen object-cover absolute left-0"
-      />
+      /> */}
       <div className="w-full sm:h-screen h-auto absolute top-0 left-0 bg-gradient-to-t from-black to-[#0ED4FF33]/20 z-10"></div>
       <div className="container mx-auto mt-[216px] flex flex-col gap-y-[180px]">
         <div className="flex flex-row gap-x-[90px]">
