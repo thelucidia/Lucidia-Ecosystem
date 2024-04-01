2f1a838966308b73c30a1ffc4b24859fe5445bfa
mirror0000 <poxii2001@gmail.com>
2024-04-01 14:46:12 +0200

    chore:remove unnessary things in post component

--- src/views/Apps/Home/Post.tsx
+++ src/views/Apps/Home/Post.tsx
@@ -1,5 +1,5 @@
-import { motion, px } from 'framer-motion';
-import React, { useEffect, useRef, useState } from 'react';
+import { motion } from 'framer-motion';
+import React, { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick-theme.css';
